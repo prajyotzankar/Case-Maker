d5cb4d8e703c7ea602bd09b27cf20878a6b3565c
prajyotzankar <zankarprajyotsushil@gmail.com>
2024-01-20 20:29:28 +0530

    Added Dev Tool Panel

--- DevToolPanel/caseMakerPanel.js
+++ DevToolPanel/caseMakerPanel.js
@@ -1,1 +1,15 @@
+chrome.devtools.panels.create(
+  "Case Maker Panel",
+  "assets/logo.png",
+  "DevToolPanel/caseMakerPanel.html",
+  (panel) => {
+    console.log("Panel created", panel);
+  }
+);
 
+const autoFillData = () => {
+  const nameElement = document.querySelectorAll('input[type="text"]')[0];
+  nameElement.value = "Ankit Singh";
+  chrome.devtools.inspectedWindow.eval("document.body.innerHTML");
+  console.log("Auto Fill Data");
+};
